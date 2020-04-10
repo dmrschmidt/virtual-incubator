5f47ca9dba1fe39c8abc9e7cd63c9b3a1a654d31
Dennis Schmidt <dmrschmidt@gmail.com>
2020-04-10 21:19:14 +0200

    sampled tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,20 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
+
+  def tags
+    [location.sample, phase.sample, industry.sample]
+  end
+
+  def location
+    ["Cairo, Egypt", "São Paulo, Brazil"]
+  end
+
+  def phase
+    ["pre-seed", "MVP", "Series-A"]
+  end
+
+  def industry
+    ["EdTech", "FinTech", "Healthcare"]
+  end
 end
