970857f376cfa5747aa7f8cecd2bd70509b5b5cb
Dennis Schmidt <dmrschmidt@gmail.com>
2020-04-23 16:10:58 +0200

    use unique gravatar images

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,6 +1,6 @@
 module WelcomeHelper
   def gravatar_url(user, size=200)
     md5 = Digest::MD5.hexdigest user.email
-    "https://www.gravatar.com/avatar/#{md5}?s=200"
+    "https://www.gravatar.com/avatar/#{md5}?s=200&d=identicon"
   end
 end
